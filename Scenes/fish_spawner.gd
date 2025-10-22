d54fa473d8a60624314fa890ac32b73f9f854478
Saman Ashoori <saman@samanashoori.com>
2025-10-23 00:25:08 +0100

    Updated fish spawning mechanic

--- Scenes/fish_spawner.gd
+++ Scenes/fish_spawner.gd
@@ -4,13 +4,16 @@ extends Node2D
 var fish_count = 0
 
 func _spawn_random_item() -> void:
+	var random_distance := randf_range(-300, 300.0)
+	var direction := Vector2(1,0)
 	var fish_scene = possible_fish.pick_random()
 	var fish: Node2D = fish_scene.instantiate()
 	add_child(fish)
+	fish.position.y += random_distance
 	
 
 func _process(delta: float) -> void:
-	if fish_count == 0:
+	if fish_count < 2:
 		_spawn_random_item()
 		fish_count += 1
 	else:
